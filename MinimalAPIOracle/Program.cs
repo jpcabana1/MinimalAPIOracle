0392fc723f95258e4ca5455dd405bd30c84d30d1
João Pedro <jpcabana1@gmail.com>
2022-07-03 14:14:22 -0300

    Raw Query

--- MinimalAPIOracle/Program.cs
+++ MinimalAPIOracle/Program.cs
@@ -1,15 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPIOracle.Config;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-string StrCon = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=GabinatorMobile)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=XEPDB1)));User Id=jpcabana;Password=otis2016;";
+string StrConEnv = builder.Configuration.GetConnectionString("XEPDB1");
 
 builder.Services.AddDbContext<Contexto>
-    (options => options.UseOracle(StrCon));
+    (options => options.UseOracle(StrConEnv));
 
 
 var app = builder.Build();
@@ -21,7 +21,46 @@ app.MapGet("Countries",async (Contexto contexto) =>
      return await contexto.Countries.ToListAsync();
 });
 
+app.MapGet("Products", async (Contexto contexto) =>
+{
+    using (var command = contexto.Database.GetDbConnection().CreateCommand())
+    {
+        command.CommandText =
+                     "SELECT "
+                    + "    cus.customer_id, "
+                    + "    ord.order_id, "
+                    + "    ord.status,"
+                    + "    ordi.unit_price, "
+                    + "    ordi.quantity, "
+                    + "    prod.description "
+                    + "FROM CUSTOMERS cus "
+                    + "INNER JOIN ORDERS ord "
+                    + "    ON cus.customer_id = ord.customer_id "
+                    + "LEFT JOIN ORDER_ITEMS ordi "
+                    + "    ON ord.order_id = ordi.order_id "
+                    + "INNER JOIN PRODUCTS prod "
+                    + "    ON ordi.product_id = prod.product_id ";
 
+        command.CommandType = CommandType.Text;
+        contexto.Database.OpenConnection();
+
+        using (var result = command.ExecuteReader())
+        {
+            var dataTable = new DataTable();
+            dataTable.Load(result);
+            
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine($"CUSTOMER_ID: {row["customer_id"]}, STATUS: {row["status"]}, DESCRIPTION: {row["description"]}");
+            }
+        }
+
+
+
+
+    }
+    return await contexto.Countries.ToListAsync();
+});
 
         
 

b0bceb16e72a51199553f7368344fabbdb6684c4
João Pedro <jpcabana1@gmail.com>
2022-07-05 12:56:20 -0300

    LINQ service

--- MinimalAPIOracle/Models/ProductDetailsDAO.cs
+++ MinimalAPIOracle/Models/ProductDetailsDAO.cs
@@ -5,8 +5,8 @@
         public decimal CustomerId { get; set; }
         public decimal OrderId { get; set; }
         public string Status { get; set; }
-        public double UnitPrice { get; set; }
-        public double Quantity { get; set; }
-        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Quantity { get; set; }
+        public string? Description { get; set; }
     }
 }
